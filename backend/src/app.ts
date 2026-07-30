c39f9e589acc6fb8fdcb2a73783f6e03d409d27e
ngoc <ngocthangn1994@gmail.com>
2026-07-30 14:09:48 -0500

    commit cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,10 +8,24 @@ import env from "./config/env";
 import { Request, Response, NextFunction } from "express";
 
 const app = express();
+
+const allowedOrigins = [
+  "https://homedevicecontrol.com",
+  "https://www.homedevicecontrol.com",
+];
 app.use(express.json());
 app.use(
   cors({
-    origin: env.CLIENT,
+    origin: (origin, callback) => {
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error(`CORS blocked origin: ${origin}`));
+    },
     credentials: true,
   }),
 );
