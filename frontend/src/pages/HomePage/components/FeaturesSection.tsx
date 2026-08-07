e00a911dc0ce4ece147fb0f9f560c806042a80d2
ngoc <ngocthangn1994@gmail.com>
2026-08-07 14:32:01 -0500

    Features section update for responsible screen

--- frontend/src/pages/HomePage/components/FeaturesSection.tsx
+++ frontend/src/pages/HomePage/components/FeaturesSection.tsx
@@ -7,28 +7,28 @@ function FeaturesSection() {
   const featuresMenu = [
     {
       icon: (
-        <FaHouseDamage className="w-20 h-20 p-3 rounded-full bg-yellow-100 text-yellow-600" />
+        <FaHouseDamage className="w-15 h-15 md:w-20 md:h-20 p-3 rounded-full bg-yellow-100 text-yellow-600" />
       ),
       name: "Control Everything",
       description: "Mange lights, locks, cameras, and more from one place.",
     },
     {
       icon: (
-        <FaShieldAlt className="w-20 h-20 p-3 rounded-full bg-orange-100 text-orange-600" />
+        <FaShieldAlt className="w-15 h-15 md:w-20 md:h-20 p-3 rounded-full bg-orange-100 text-orange-600" />
       ),
       name: "Advanced Security",
       description: "Real-time alrts, smart blocks, and 24/7 protection.",
     },
     {
       icon: (
-        <SlEnergy className="w-20 h-20 p-3 rounded-full bg-slate-100 text-silver-600" />
+        <SlEnergy className="w-15 h-15 md:w-20 md:h-20 p-3 rounded-full bg-slate-100 text-silver-600" />
       ),
       name: "Save Energy",
       description: "Monitor usage and automate to lower your bills",
     },
     {
       icon: (
-        <IoIosPhonePortrait className="w-20 h-20 p-3 rounded-full bg-green-100 text-green-600" />
+        <IoIosPhonePortrait className="w-15 h-15 md:w-20 md:h-20 p-3 rounded-full bg-green-100 text-green-600" />
       ),
       name: "Access Anywhere",
       description: "Stay connected and in control from your phone.",
@@ -36,7 +36,7 @@ function FeaturesSection() {
   ];
   return (
     <>
-      <section className="space-y-5 px-50 text-slate-600 mt-10">
+      <section className="space-y-5 text-slate-600 mt-10">
         <div className="text-center">
           <span className="bg-slate-100 text-xl text-blue-600 font-medium px-2 py-2 rounded-xl">
             Features
@@ -45,11 +45,11 @@ function FeaturesSection() {
             Everything You Need for a Smarter Home
           </p>
         </div>
-        <div className="grid grid-cols-4 gap-4 mt-10">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-10 px-5">
           {featuresMenu.map((item) => (
             <div className="border border-slate-200 flex flex-col items-center text-center p-5 rounded-xl shadow-sm">
               <p>{item.icon}</p>
-              <div className="w-xs">
+              <div className="w-full">
                 <p className="font-bold">{item.name}</p>
                 <p>{item.description}</p>
               </div>
